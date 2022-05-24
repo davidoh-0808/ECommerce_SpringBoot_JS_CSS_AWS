3507ea4888a1ffd33b7aef23ff10497079e47d66
David Oh <davidoh72@gmail.com>
2022-05-24 20:11:05 +0900

    22.05.24_(1)Spring Boot Initializr to start the project + (2)application.properties to set up Data Source Bean using DB Oracle 21c and JPA

---
+++ src/main/java/com/application/gentlegourmet/GentlegourmetApplication.java
@@ -0,0 +1,13 @@
+package com.application.gentlegourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentlegourmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GentlegourmetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/gentlegourmet/GentlegourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.gentlegourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GentlegourmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
