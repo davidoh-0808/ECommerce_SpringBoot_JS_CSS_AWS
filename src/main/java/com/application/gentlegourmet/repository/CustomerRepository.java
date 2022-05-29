7c1e0ca1f31d3df68c007450b2c262132a461677
David Oh <davidoh72@gmail.com>
2022-05-29 20:11:04 +0900

    22.05.29_Adding Spring Security_in Progress

--- src/main/java/com/application/gentlegourmet/repository/CustomerRepository.java
+++ src/main/java/com/application/gentlegourmet/repository/CustomerRepository.java
@@ -2,9 +2,12 @@ package com.application.gentlegourmet.repository;
 
 import com.application.gentlegourmet.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-
+    //add the following method for Spring Security ->
+    @Query("SELECT c FROM Customer c WHERE c.username = ?1")
+    public Customer findByUsername(String username);
 
 }
--- src/main/java/com/application/gentlegourmet/service/CustomerDetailsService.java
+++ src/main/java/com/application/gentlegourmet/service/CustomerDetailsService.java
@@ -1,14 +1,24 @@
 package com.application.gentlegourmet.service;
 
+import com.application.gentlegourmet.entity.Customer;
+import com.application.gentlegourmet.entity.CustomerUserDetails;
+import com.application.gentlegourmet.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@RequiredArgsConstructor
 public class CustomerDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Customer customer = customerRepository.findByUsername(username);
+        if(customer == null) throw new UsernameNotFoundException("No Customer found with this username : " + username);
+
+        return new CustomerUserDetails(customer);
     }
 
 }
