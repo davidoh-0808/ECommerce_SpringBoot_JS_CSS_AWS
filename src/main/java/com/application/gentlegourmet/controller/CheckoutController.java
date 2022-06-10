15b397137cf74393d8c21ac88b181415ddd466f0
David Oh <davidoh72@gmail.com>
2022-06-11 07:58:57 +0900

    22.06.10_checkout page_checkout module_in progress

--- src/main/java/com/application/gentlegourmet/controller/CheckoutController.java
+++ src/main/java/com/application/gentlegourmet/controller/CheckoutController.java
@@ -12,7 +12,7 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -62,6 +62,23 @@ public class CheckoutController {
             return "redirect:" + referer;
         }
 
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @PostMapping("/payments/complete")
+    public String handleCheckout(Model model, RedirectAttributes redirectAttr, HttpServletRequest request) {
+        // common variable for page redirect
+        String referer = request.getHeader("referer");
+
+
+        System.out.println("*************** handleCheckout called ***************");
+
+
+
+        redirectAttr.addFlashAttribute("successMessage", "Payment Complete! Thank you :)");
+
+        return "redirect:" + referer;
     }
 
 
